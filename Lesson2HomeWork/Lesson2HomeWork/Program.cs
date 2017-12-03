1fbe8db3cc60d0680f1d248e028503d5ea8af4b1
Vladimir <vovanpadawan@gmail.com>
2017-12-03 23:54:51 +0300

    Добавил домашку: 3 задачи из 7 для второго урока.

---
+++ Lesson2HomeWork/Lesson2HomeWork/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Написать метод возвращающий минимальное из трех чисел.
+//2. Написать метод подсчета количества цифр числа.
+
+namespace Lesson2HomeWork
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Возвращает минимальное число из трёх: 105, 50, 5");
+            Console.WriteLine(MinOfThree(105, 50, 5));
+
+            Console.WriteLine("Считает и возвращает количество разрядов в числе 114");
+            Console.WriteLine("Result of 114: {0}", DigitNum(114));
+
+            Console.ReadKey();
+        }
+
+
+        // 1.
+        /// <summary>
+        /// Возвращает минимальное из трёх переданных целых
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        static int MinOfThree(int a, int b, int c)
+        {
+            if (a < b & a < c) return a;
+            else if (b < c) return b;
+            else return c;
+        }
+
+        // 2.
+        /// <summary>
+        /// Считает количество разрядов целого числа
+        /// Возвращает так же целое число
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        static int DigitNum(int num)
+        {
+            int digit = 0;
+            //если пользователь вводит отрицательное число
+            num = Math.Abs(num);
+            do
+            {
+                digit++; //в первом входе инкрементирует разряд всегда
+                num /= 10;
+            } while (num > 0); //повторяет цикл, если после деления на 10 что-то осталось 
+
+            return digit;
+        }
+
+    }
+}
---
+++ Lesson2HomeWork/Task3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+// 3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Программа подсчитывает сумму всех нечетных положительных чисел, которые вводит пользователь.\n");
+
+            int num = 0;
+            int result = 0;
+            do
+            {
+                WriteLine("Введите целое число для расчёта, или 0 для вывода результата");
+                num = GetNumber();
+                WriteLine($"Вы ввели {num}");
+
+                if (num == 0) break;
+
+                if (num > 0 && num % 2 > 0) result += num;
+
+            } while (num != 0);
+
+            WriteLine($"\nРезультат: {result}");
+            ReadKey();
+        }
+
+        /// <summary>
+        /// Метод для получения числового значения из строки.
+        /// </summary>
+        /// <returns></returns>
+        static int GetNumber()
+        {
+            int result = 0;
+            bool success = false;
+            string input = "";
+
+            while (success == false)
+            {
+                input = ReadLine();
+                success = Int32.TryParse(input, out result);
+                if (success == false)
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("ОШИБКА: введенное значение не является целым числом, попробуйте ещё раз.");
+                    ResetColor();
+                }
+            }
+            return result;
+        }
+    }
+}

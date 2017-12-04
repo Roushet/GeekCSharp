e288ffa84c82c8ed9df3a0432850ef48e4168716
Vladimir <vovanpadawan@gmail.com>
2017-12-04 23:37:49 +0300

    Сделал задание 6

---
+++ Lesson2HomeWork/Task5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task5
+{
+
+    //5. а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или все в норме;
+
+    //б) * Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float lowerBound = 18.5F; // нижняя граница нормы
+            float upperBound = 24.9F; // верхняя граница нормы
+
+            WriteLine("Программа считает индекс массы тела и выдаёт совет по коррекции.\n");
+            WriteLine("Введите ваш вес в кг.");
+            int weight = GetNumber();
+
+            WriteLine("Введите ваш рост в см.");
+            int height = GetNumber();
+            float bmi = BMI(weight, height);
+            WriteLine("Ваш идекс массы тела: {0:#.##}", bmi);
+
+            //вес ниже нормы
+            if (bmi < lowerBound)
+            {
+                WriteLine("Вы слишком худы для своего веса.");
+                //5б
+                WriteLine("Вам нужно набрать как минимум {0:#.##} кг.", lowerBound * Math.Pow((float)height / 100, 2) - (float)weight);
+            }
+
+            //вес выше нормы
+            if (bmi > upperBound)
+            {
+                WriteLine("У вас лишний вес.");
+                //5б
+                WriteLine("Вам сбросить как минимум {0:#.##} кг.", (float)weight - upperBound * Math.Pow((float)height / 100, 2));
+            }
+            
+            //Вес в пределах нормы
+            if (bmi <= upperBound && bmi >= lowerBound) Console.WriteLine("Ваш индекс массы тела в пределах нормы. Всё в порядке.");
+
+            ReadKey();
+
+        }
+
+        /// <summary>
+        /// Метод для получения числового значения из строки.
+        /// </summary>
+        /// <returns></returns>
+        static int GetNumber()
+        {
+            int result = 0;
+            bool success = false;
+            string input = "";
+
+            while (success == false)
+            {
+                input = ReadLine();
+                success = Int32.TryParse(input, out result);
+                if (success == false)
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine("ОШИБКА: введенное значение не является целым числом, попробуйте ещё раз.");
+                    ResetColor();
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Возвращает индекс массы тела
+        /// </summary>
+        /// <param name="bodyMass">масса тела в кг.</param>
+        /// <param name="height">рост в см.</param>
+        /// <returns></returns>
+        static float BMI(int bodyMass, int height)
+        {
+            float heightM = (float)height / 100;
+
+            return (bodyMass / (heightM * heightM));
+        }
+    }
+}
---
+++ Lesson2HomeWork/Task6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    //6. * Написать программу подсчета количества “Хороших” чисел в диапазоне от 1 до 1 000 000 000. 
+    //Хорошим называется число, которое делится на сумму своих цифр. 
+    //Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            //{
+            //    int num = GetNumber();
+            //    Console.WriteLine("Sum: {0}", SumOfDigits(num));
+            //}
+
+            //Console.ReadLine();
+            int result = 0;
+            for (int i = 1; i <= 1000000000; i++)
+            {
+                if(IsGoodNum(i)) result++;
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static bool IsGoodNum(int num)
+        {
+            if (num % SumOfDigits(num) == 0) return true;
+            else return false;
+
+        }
+
+        static int SumOfDigits(int num)
+        {
+            int result = 0;
+
+            do
+            {
+                if (num % 10 > 0)
+                {
+                    result += num % 10;
+                    num /= 10;
+                }
+                else if (num < 10)
+                {
+                    result += num; //берём последний оставшийся разряд и складываем в итог, 
+                    num = 0; //затем обнуляем число, чтобы выйти из цикла
+                }
+                else num /= 10; //если число с нулём, то просто пропускаем его
+
+            } while (num > 0);
+
+            return result;
+        }
+
+        static int GetNumber()
+        {
+            int result = 0;
+            bool success = false;
+            string input = "";
+
+            while (success == false)
+            {
+                input = Console.ReadLine();
+                success = Int32.TryParse(input, out result);
+                if (success == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ОШИБКА: введенное значение не является целым числом, попробуйте ещё раз.");
+                    Console.ResetColor();
+                }
+            }
+            return result;
+        }
+    }
+}

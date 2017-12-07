8ff966f724e4df0bf24f0780c0bfde55b8edb150
Vladimir <vovanpdawan@gmail.com>
2017-12-07 19:05:37 +0300

    Начал делать домашку на субботу

---
+++ Task1/Lesson3HomeWork/Complex.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3HomeWork
+{
+    class Complex
+    {
+        double im;
+        double re;
+
+        public Complex​()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        public Complex​(double _im​, double _re)
+        {
+            im = _im;
+            re = _re;
+        }
+
+        public Complex Plus​(Complex x2)
+        {
+            Complex x3 = new Complex​();
+            x3​.im = x2​.im + im;
+            x3​.re = x2​.re + re;
+            return x3;
+        }
+
+        public Complex Minus(Complex x2)
+        {
+            Complex x3 = new Complex​();
+            x3​.im = x2​.im - im;
+            x3​.re = x2​.re - re;
+            return x3;
+        }
+
+        public double Im
+        {
+            get { return im​; }
+
+            set { if (value >= 0) im = value; }
+        }
+
+        public override string ToString​()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+
+
+
+
+}
---
+++ Task1/Lesson3HomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3HomeWork
+{
+
+    //1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры;
+
+    //б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex num1 = new Complex(1, 2);
+
+            Complex num2 = new Complex(3, 5);
+
+           num1.Plus(num2);
+        }
+    }
+}

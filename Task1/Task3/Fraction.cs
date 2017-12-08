fd8a2a10d67d589eab5ab63b4bebd5a266da37ac
Vladimir <vovanpadawan@gmail.com>
2017-12-09 00:11:37 +0300

    Сделал последнее задание

--- Task1/Task3/Fraction.cs
+++ Task1/Task3/Fraction.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Task3
@@ -9,19 +10,151 @@ namespace Task3
     class Fraction
     {
         //числитель
-        int numerator = 0;
+        int numerator;
         //знаменатель
         //не может быть нулём
-        int denominator = 1;
+        int denominator;
 
-        //конструктор
+        /// <summary>
+        /// Конструктор по-умолчанию
+        /// </summary>
+        public Fraction()
+        {
+            numerator = 0;
+            denominator = 1;
+        }
+
+        /// <summary>
+        /// Перегрузка конструктора по умолчанию
+        /// </summary>
+        /// <param name="_numerator"></param>
+        /// <param name="_denominator"></param>
         public Fraction(int _numerator, int _denominator)
         {
-            //заранее страхуемся от ошибки деления на 0
             this.numerator = _numerator;
-            if (_denominator == 0)
-                this.denominator = 1;
-            else this.denominator = _denominator;
+            this.denominator = _denominator;
         }
+
+        public int Numerator 
+        {
+            get { return numerator; }
+            set { numerator = value; }
+        }
+
+        public int Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+
+        /// <summary>
+        /// Перегрузка стандартного метода для класса дробей
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (Numerator == 0) return "0";
+            if (Numerator <0 || Denominator < 0) return "- " + Math.Abs(Numerator) + " / " + Math.Abs(Denominator);
+            return Numerator + " / " + Denominator;
+        }
+
+        //перегрузка оператора сложения +
+        public static Fraction operator +(Fraction x, Fraction y)
+        {
+            int gcd = 1;
+            Fraction result = new Fraction();
+
+            result.numerator = x.numerator * y.denominator + y.numerator*x.denominator;
+            result.denominator = x.denominator* y.denominator;
+
+            gcd = GCD(result.numerator, result.denominator);
+
+            result.numerator /= gcd;
+            result.denominator /= gcd;
+
+            return result;
+        }
+
+        //перегрузка оператора вычитания -
+        public static Fraction operator -(Fraction x, Fraction y)
+        {
+            int gcd = 1;
+            Fraction result = new Fraction();
+
+            result.numerator = x.numerator * y.denominator - y.numerator * x.denominator;
+            result.denominator = x.denominator * y.denominator;
+
+            gcd = GCD(result.numerator, result.denominator);
+
+            result.numerator /= gcd;
+            result.denominator /= gcd;
+
+            return result;
+        }
+
+        //перегрузка оператора умножения *
+        public static Fraction operator *(Fraction x, Fraction y)
+        {
+            int gcd = 1;
+            Fraction result = new Fraction();
+
+            result.numerator = x.numerator * y.numerator;
+            result.denominator = x.denominator * y.denominator;
+
+            gcd = GCD(result.numerator, result.denominator);
+
+            result.numerator /= gcd;
+            result.denominator /= gcd;
+
+            return result;
+
+        }
+
+        //перегрузка оператора деления -
+        public static Fraction operator /(Fraction x, Fraction y)
+        {
+            int gcd = 1;
+            Fraction result = new Fraction();
+
+            result.numerator = x.numerator * y.denominator;
+            result.denominator = x.denominator * y.numerator;
+
+            gcd = GCD(result.numerator, result.denominator);
+
+            result.numerator /= gcd;
+            result.denominator /= gcd;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Возвращает наибольший общий делитель (НОД)
+        /// Сервисный метод для всех операций с дробями
+        /// Реализация спёрта из методички
+        /// </summary>
+        /// <returns></returns>
+        public static int GCD(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            if (a == 0)
+                return b;
+            else
+                return a;
+        }
+
+        //реализация НОД через рекурсию и тренарный оператор
+        //версия для наркоманов
+
+        //static int GCD(int a, int b)
+        //{
+        //    return b == 0 ? a : GCD(b, a % b);
+        //}
     }
 }
--- Task1/Task3/Program.cs
+++ Task1/Task3/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace Task3
 {
+
+//Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел.
+//Предусмотреть методы сложения, вычитания, умножения и деления дробей.
+//Написать программу, демонстрирующую все разработанные элементы класса.
+
     class Program
     {
         static void Main(string[] args)
         {
+            Fraction x1 = new Fraction(1, 3);
+
+            Fraction x2 = new Fraction(3, 11);
+
+            Fraction x3 = new Fraction();
+
+            Console.WriteLine($"Используются дроби: {x1.ToString()} и {x2.ToString()}");
+
+            Console.WriteLine($"Сложение: {x1.ToString()} + {x2.ToString()} = ");
+            x3 = x1 + x2;
+            Console.WriteLine(x3.ToString());
+
+            Console.WriteLine($"Вычитание: {x1.ToString()} - {x2.ToString()} = ");
+            x3 = x1 - x2;
+            Console.WriteLine(x3.ToString());
+
+            Console.WriteLine($"Умножение: {x1.ToString()} * {x2.ToString()} = ");
+            x3 = x1 * x2;
+            Console.WriteLine(x3.ToString());
+
+            Console.WriteLine($"Деление: {x1.ToString()} / {x2.ToString()} = ");
+            x3 = x1 / x2;
+            Console.WriteLine(x3.ToString());
+
+            Console.ReadKey();
+
+
         }
     }
 }

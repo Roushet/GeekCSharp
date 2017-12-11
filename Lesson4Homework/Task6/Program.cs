4fb85f2f9b07f722e949e0677f07496ba1c6bd5d
Vladimir <vovanpdawan@gmail.com>
2017-12-11 15:39:07 +0300

    Рассовал методы по классам, кое где поменял тип. Сделал методы более самодостаточными

--- Lesson4Homework/Task6/Program.cs
+++ Lesson4Homework/Task6/Program.cs
@@ -8,24 +8,34 @@ using System.Threading.Tasks;
 
 namespace Task6
 {
+
+    //Владимир Евдокимов
+        //6. *** Написать игру “Верю.Не верю”. В файле хранятся некоторые данные и правда это или нет.Например:
+        //“Шариковую ручку изобрели в древнем Египте”, “Да”.
+
+        //Компьютер загружает эти данные, случайным образом выбирает 5 вопросов и задает их игроку.
+        //Игрок пытается ответить правда или нет, то что ему загадали и набирает баллы.Список вопросов
+        //ищите во вложении или можно воспользоваться Интернетом.
+
     class Program
     {
         static void Main(string[] args)
         {
             #region Инициализация опросника
 
-            int questionCount = 5; // количество вопросов 
+            int questionCount = 5; // количество вопросов, можно менять, главное что было было меньше 210
+
             int[] numbers = new int[questionCount]; // массив для номеров вопросов
             string rawData; //строка неразобранного текста опросника
             string[] lines; //массив разобранного по строкам текста опросника
 
             QuestionData[] questions = new QuestionData[questionCount]; //создаётся массив вопросов
+            rawData = QuestionData.GetQuestionsFromAssembly(); //получаются вопросы из сборки
 
-            rawData = GetQuestionsFromAssembly(); //получаются вопросы из сборки
             lines = rawData.Split(new char[] { '\n' }); //разбиваются по линиям
-            numbers = GetFiveRandoms(lines.Length); //получаются уникальные номера вопросов
+            numbers = GetRandomInts(lines.Length, questionCount); //получаются уникальные номера вопросов
 
-            questions = InitQuestionData(questions, lines, numbers); //массив вопросов инициализируется подготовленными данными
+            questions = QuestionData.InitQuestionData(questions, lines, numbers); //массив вопросов инициализируется подготовленными данными
 
             #endregion
 
@@ -34,15 +44,15 @@ namespace Task6
             int userScore = 0;
             int progress = 0;
             bool reply = false;
-            
+
             Console.WriteLine("Приветствуем вас в игре \"Верю не верю\"");
             Console.WriteLine($"Вам предстоит ответить на {questionCount} вопросов.\nПриступим!");
 
             //основной цикл
-            while (progress < 5)
+            while (progress < questionCount)
             {
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Утверждение:{0}\n", questions[progress].Question);
+                Console.WriteLine("\nУтверждение:{0}\n", questions[progress].Question);
                 Console.ForegroundColor = ConsoleColor.Gray;
 
                 Console.WriteLine("Вы верите, что это утверждение правда? Введите ответ.");
@@ -50,8 +60,8 @@ namespace Task6
                 reply = GetAnswer();
                 //Обработка ответа
                 if (
-                    (reply && questions[progress].Answer == "Да") ||
-                    (!reply && questions[progress].Answer == "Нет")
+                    (reply && questions[progress].Answer) ||
+                    (!reply && !questions[progress].Answer)
                     )
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -65,13 +75,17 @@ namespace Task6
                 progress++;
             }
 
-            Console.WriteLine("Игра окончена! Ваш счёт {0}", userScore);
+            Console.WriteLine("Игра окончена! Ваш счёт {0} из {1}", userScore, questionCount);
 
             #endregion
 
             Console.ReadKey();
         }
-
+        /// <summary>
+        /// Ждёт ввода с клавиатуры.
+        /// Возращает ответ в виде переменной true(да) / false (нет)
+        /// </summary>
+        /// <returns>ответ</returns>
         static bool GetAnswer()
         {
             ConsoleKeyInfo key;
@@ -91,32 +105,28 @@ namespace Task6
                     return false;
                 }
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Ошибка ввода. Пожалуйста, вводите только 1 или 2.");
+                Console.WriteLine("\nОшибка ввода. Пожалуйста, вводите только 1 или 2.");
                 Console.ForegroundColor = ConsoleColor.White;
 
             } while (key.Key != ConsoleKey.D1 || key.Key != ConsoleKey.D2);//странно, но неравенство != не работает с енумератором КонсольКей
             return false;
         }
 
-        static string GetQuestionsFromAssembly()
+
+        /// <summary>
+        /// Возвращает список из 5 уникальных интов, от 0 до параметра
+        /// которые потом 
+        /// будут номерами строк при выборе вопросов из файла
+        /// </summary>
+        /// <param name="maxIndex">Максимальное значение интервала для рандома</param>
+        /// <returns></returns>
+        static int[] GetRandomInts(int maxIndex, int size)
         {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            string resourceName = "Task6.Questions.txt";
-            Stream resourceStream = assembly.GetManifestResourceStream(resourceName);
-
-            StreamReader streamReader = new StreamReader(resourceStream);
-            string file = streamReader.ReadToEnd();
-
-            return file;
-        }
-
-        static int[] GetFiveRandoms(int maxIndex)
-        {
-            int[] fiveRandomInts = new int[5];
+            int[] fiveRandomInts = new int[size];
             Random rnd = new Random();
             int temp = 0;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < size; i++)
             {
                 temp = rnd.Next(0, maxIndex);
                 while (CheckArray(fiveRandomInts, temp))
@@ -129,6 +139,13 @@ namespace Task6
             return fiveRandomInts;
         }
 
+        /// <summary>
+        /// Вспомогательный метод, который проверяет наличие числа в массиве
+        /// Нужен для того, чтобы добавлять в массив только уникальные числа
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
         static bool CheckArray(int[] array, int num)
         {
             foreach (int i in array)
@@ -138,20 +155,6 @@ namespace Task6
             return false;
         }
 
-        static QuestionData[] InitQuestionData(QuestionData[] qData, string[] rawData, int[] numbers)
-        {
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                string[] rawLine = rawData[numbers[i]].Split(new string[] { ";" }, StringSplitOptions.None);
-                qData[i] = new QuestionData();
-
-                qData[i].Question = rawLine[0];
-                qData[i].Answer = rawLine[1];
-                qData[i].Description = rawLine[2];
-            }
-
-            return qData;
-        }
     }
 }
--- Lesson4Homework/Task6/Question.cs
+++ Lesson4Homework/Task6/Question.cs
@@ -2,28 +2,81 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Task6
 {
+    /// <summary>
+    /// Класс, в котором описан тип Вопрос и методы для его заполнения
+    /// </summary>
     class QuestionData
     {
         private string question;
-        private string answer;
+        private bool answer;
         private string description;
 
         public string Question { get => question; set => question = value; }
-        public string Answer { get => answer; set => answer = value; }
+        public bool Answer { get => answer; set => answer = value; }
         public string Description { get => description; set => description = value; }
 
+        /// <summary>
+        /// Конструктор по-умолчанию
+        /// </summary>
         public QuestionData()
         {
             question = "";
-            answer = "";
+            answer = false;
             description = "";
         }
 
+        /// <summary>
+        /// Загружает вопросы из сборки, т.е. текстовичка Questions, который 
+        /// вшит в тело программы
+        /// </summary>
+        /// <returns>строку</returns>
+        public static string GetQuestionsFromAssembly()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "Task6.Questions.txt";
+            Stream resourceStream = assembly.GetManifestResourceStream(resourceName);
+
+            StreamReader streamReader = new StreamReader(resourceStream);
+            string file = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            return file;
+        }
+
+        /// <summary>
+        /// Метод, который инициализиует массив вопросов
+        /// и заполняет его передаваемыми данными
+        /// </summary>
+        /// <param name="qData">Пустой массив вопросов</param>
+        /// <param name="rawData">Массив строк, где каждая строка - один вопрос</param>
+        /// <param name="numbers">Массив неповторяющися целых чисел</param>
+        /// <returns></returns>
+        public static QuestionData[] InitQuestionData(QuestionData[] qData, string[] rawData, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string[] rawLine = rawData[numbers[i]].Split(new string[] { ";" }, StringSplitOptions.None);
+                qData[i] = new QuestionData();
+
+                qData[i].Question = rawLine[0];
+
+                if (rawLine[1] == "Да")
+                {
+                    qData[i].Answer = true;
+                }
+                else qData[i].Answer = false;
+
+                qData[i].Description = rawLine[2];
+            }
+
+            return qData;
+        }
     }
 
 }

e5572ec38d9ac86f254c49a323b12663bc8c0ba8
Vladimir <vovanpdawan@gmail.com>
2017-12-11 19:07:01 +0300

    Начал делать упражнение 5

---
+++ Lesson4Homework/Task5/Doubler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    //Реализовать класс “Удвоитель”. Класс хранит в себе поле current - текущее значение, finish - число,
+    //которого нужно достичь, конструктор, в котором задается конечное число.Методы:
+    //увеличить число на 1, увеличить число в два раза, сброс текущего до 1, свойство Current,
+    //которое возвращает текущее значение, свойство Finish, которое возвращает конечное значение.
+    //Создать с помощью этого класса игру, в которой компьютер загадывает число, а человек.выбирая из меню на экране,
+    //отдает команды удвоителю и старается получить это число за наименьшее число ходов.Если человек получает число больше
+    //положенного, игра прекращается.
+
+    class Doubler
+    {
+        int _current;
+        int _finish;
+
+        public Doubler()
+        {
+            _current = 1;
+            _finish = 1;
+        }
+
+        public int finish
+        {
+            get => _finish;
+            set => _finish = value;
+        }
+
+        public int AddOne()
+        {
+            return _current += 1;
+        }
+
+        public int Double()
+        {
+            return _current *= 2;
+        }
+
+        public int Reset()
+        {
+            return _current = 1;
+        }
+    }
+}

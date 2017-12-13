ab89f665ae5aa1f816951ff28a1a7d1d1b3b7612
Vladimir <vovanpdawan@gmail.com>
2017-12-13 20:31:44 +0300

    Начал новую домашку

---
+++ Lesson5/Task4/Pupil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Pupil
+    {
+        string _name;
+        string _surename;
+        int[] _grades;
+
+        public Pupil()
+        {
+            _name = "";
+            _surename = "";
+            _grades = new int[3];
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surename
+        {
+            get { return _surename; }
+            set { _surename = value; }
+        }
+
+
+    }
+}

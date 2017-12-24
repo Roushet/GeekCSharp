2203a70448fc455fb396c9fb12f3e5cd9b95a105
Vladimir <vovanpadawan@gmail.com>
2017-12-24 22:36:29 +0300

    На сегодня всё. Формируется поле, ставятся временные точки, есть нерабочий алгоритм поиска.

--- Lesson7Homework/Task1/Cell.cs
+++ Lesson7Homework/Task1/Cell.cs
@@ -22,6 +22,7 @@ namespace Task1
         delegate void ClickHandler(object sender, EventArgs e, Cell cell);
 
         public static List<Cell> Cells = new List<Cell>();
+        public static Cell[,] Field;
 
         private int cellSize = 45;
         Button button;
@@ -30,6 +31,8 @@ namespace Task1
         int column;
         //Color color;
         bool isWall;
+        bool isCat;
+        bool isExit;
 
         public Color color
         {
@@ -63,22 +66,26 @@ namespace Task1
         public void MakeField(int size, Control.ControlCollection controls)
         {
             Point location = new Point(10, 30);
+            Field = new Cell[size, size];
 
-            for (int i = 1; i < size; i++)
+            for (int i = 1; i <= size; i++)
             {
-                for (int j = 1; j < size; j++)
+                for (int j = 1; j <= size; j++)
                 {
                     Cell cell = new Cell(controls, location, i, j);
-                    cell.button.Click += delegate (object sender2, EventArgs e2) { OnCellClick(sender2, e2, cell); };
-                    Cells.Add(cell);
-
-
+                    Field[i - 1, j - 1] = cell;
                     location.X += cellSize;
 
+                    cell.button.Click += delegate (object sender2, EventArgs e2) { OnCellClick(sender2, e2, cell); };
                 }
                 location.X = 10;
                 location.Y += cellSize;
             }
+            Field[0, 0].isCat = true;
+            Field[0, 0].color = Color.Black;
+            Field[size-1, size-1].isExit = true;
+            Field[size-1, size-1].color = Color.Crimson;
+
         }
 
         private void OnCellClick(object sender, EventArgs e, Cell cell)
@@ -105,12 +112,48 @@ namespace Task1
             Cells.Clear();
         }
 
-        //public void CellClicked(object sender, CellClickEventArgs args)
-        //{
+
+        public void FindWave(int startX, int startY, int targetX, int targetY)
+        {
+            bool add = true;
+            int[,] cMap = new int[MapHeight, MapWidht];
+            int x, y, step = 0;
+            for (y = 0; y < MapHeight; y++)
+                for (x = 0; x < MapWidht; x++)
+                {
+                    if (Map[y, x] == 1)
+                        cMap[y, x] = -2;//индикатор стены
+                    else
+                        cMap[y, x] = -1;//индикатор еще не ступали сюда
+                }
+            cMap[targetY, targetX] = 0;//Начинаем с финиша
+            while (add == true)
+            {
+                add = false;
+                for (y = 0; y < MapWidht; y++)
+                    for (x = 0; x < MapHeight; x++)
+                    {
+                        if (cMap[x, y] == step)
+                        {
+                            //Ставим значение шага+1 в соседние ячейки (если они проходимы)
+                            if (y - 1 >= 0 && cMap[x - 1, y] != -2 && cMap[x - 1, y] == -1)
+                                cMap[x - 1, y] = step + 1;
+                            if (x - 1 >= 0 && cMap[x, y - 1] != -2 && cMap[x, y - 1] == -1)
+                                cMap[x, y - 1] = step + 1;
+                            if (y + 1 < MapWidht && cMap[x + 1, y] != -2 && cMap[x + 1, y] == -1)
+                                cMap[x + 1, y] = step + 1;
+                            if (x + 1 < MapHeight && cMap[x, y + 1] != -2 && cMap[x, y + 1] == -1)
+                                cMap[x, y + 1] = step + 1;
+                        }
+                    }
+                step++;
+                add = true;
+                if (cMap[startY, startX] != -1)//решение найдено
+                    add = false;
+                if (step > MapWidht * MapHeight)//решение не найдено
+                    add = false;
+            }
 
 
-
-        //}
-
-    }
+        }
 }

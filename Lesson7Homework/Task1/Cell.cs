27de74f7aa7fbc7730bed7c5324e4758ec56732c
Vladimir <vovanpdawan@gmail.com>
2017-12-25 18:55:16 +0300

    немного порефакторил

--- Lesson7Homework/Task1/Cell.cs
+++ Lesson7Homework/Task1/Cell.cs
@@ -33,6 +33,8 @@ namespace Task1
         bool isWall;
         bool isCat;
         bool isExit;
+        //для поиска пути
+        int wave;
 
         public Color color
         {
@@ -105,55 +107,71 @@ namespace Task1
 
         public static void ClearAll(Control.ControlCollection controls)
         {
-            foreach (var cell in Cells)
+            if (Field != null)
             {
-                controls.Remove(cell.button);
-            }
-            Cells.Clear();
-        }
-
-
-        public void FindWave(int startX, int startY, int targetX, int targetY)
-        {
-            bool add = true;
-            int[,] cMap = new int[MapHeight, MapWidht];
-            int x, y, step = 0;
-            for (y = 0; y < MapHeight; y++)
-                for (x = 0; x < MapWidht; x++)
+                foreach (var cell in Field)
                 {
-                    if (Map[y, x] == 1)
-                        cMap[y, x] = -2;//индикатор стены
-                    else
-                        cMap[y, x] = -1;//индикатор еще не ступали сюда
+                    controls.Remove(cell.button);
                 }
-            cMap[targetY, targetX] = 0;//Начинаем с финиша
-            while (add == true)
+            }
+            Field = null;
+        }
+
+
+        public void Wave()
+        {
+            Cell start = Field[0, 0];
+            Cell finish = Field[15, 15];
+            start.wave = 0;
+
+            while (finish.wave == 0)
             {
-                add = false;
-                for (y = 0; y < MapWidht; y++)
-                    for (x = 0; x < MapHeight; x++)
-                    {
-                        if (cMap[x, y] == step)
-                        {
-                            //Ставим значение шага+1 в соседние ячейки (если они проходимы)
-                            if (y - 1 >= 0 && cMap[x - 1, y] != -2 && cMap[x - 1, y] == -1)
-                                cMap[x - 1, y] = step + 1;
-                            if (x - 1 >= 0 && cMap[x, y - 1] != -2 && cMap[x, y - 1] == -1)
-                                cMap[x, y - 1] = step + 1;
-                            if (y + 1 < MapWidht && cMap[x + 1, y] != -2 && cMap[x + 1, y] == -1)
-                                cMap[x + 1, y] = step + 1;
-                            if (x + 1 < MapHeight && cMap[x, y + 1] != -2 && cMap[x, y + 1] == -1)
-                                cMap[x, y + 1] = step + 1;
-                        }
-                    }
-                step++;
-                add = true;
-                if (cMap[startY, startX] != -1)//решение найдено
-                    add = false;
-                if (step > MapWidht * MapHeight)//решение не найдено
-                    add = false;
+                start.
             }
 
-
         }
+
+        //public void FindWave(int startX, int startY, int targetX, int targetY)
+        //{
+        //    bool add = true;
+        //    int[,] cMap = new int[MapHeight, MapWidht];
+        //    int x, y, step = 0;
+        //    for (y = 0; y < MapHeight; y++)
+        //        for (x = 0; x < MapWidht; x++)
+        //        {
+        //            if (Map[y, x] == 1)
+        //                cMap[y, x] = -2;//индикатор стены
+        //            else
+        //                cMap[y, x] = -1;//индикатор еще не ступали сюда
+        //        }
+        //    cMap[targetY, targetX] = 0;//Начинаем с финиша
+        //    while (add == true)
+        //    {
+        //        add = false;
+        //        for (y = 0; y < MapWidht; y++)
+        //            for (x = 0; x < MapHeight; x++)
+        //            {
+        //                if (cMap[x, y] == step)
+        //                {
+        //                    //Ставим значение шага+1 в соседние ячейки (если они проходимы)
+        //                    if (y - 1 >= 0 && cMap[x - 1, y] != -2 && cMap[x - 1, y] == -1)
+        //                        cMap[x - 1, y] = step + 1;
+        //                    if (x - 1 >= 0 && cMap[x, y - 1] != -2 && cMap[x, y - 1] == -1)
+        //                        cMap[x, y - 1] = step + 1;
+        //                    if (y + 1 < MapWidht && cMap[x + 1, y] != -2 && cMap[x + 1, y] == -1)
+        //                        cMap[x + 1, y] = step + 1;
+        //                    if (x + 1 < MapHeight && cMap[x, y + 1] != -2 && cMap[x, y + 1] == -1)
+        //                        cMap[x, y + 1] = step + 1;
+        //                }
+        //            }
+        //        step++;
+        //        add = true;
+        //        if (cMap[startY, startX] != -1)//решение найдено
+        //            add = false;
+        //        if (step > MapWidht * MapHeight)//решение не найдено
+        //            add = false;
+        //    }
+
+
+    }
 }

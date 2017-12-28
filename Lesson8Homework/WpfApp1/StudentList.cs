804900a02cbb639313f45257cded62cce78990fb
Vladimir <vovanpadawan@gmail.com>
2017-12-28 23:09:10 +0300

    Добавил класс лист для студентов

---
+++ Lesson8Homework/WpfApp1/StudentList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    static class StudentList
+    {
+        static List<StudentSerializable> list = new List<StudentSerializable>();
+    }
+}

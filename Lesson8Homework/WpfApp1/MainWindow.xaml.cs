9c0304021f4a6acacb282772c9103a23bedf4d09
Vladimir <vovanpadawan@gmail.com>
2017-12-28 23:06:30 +0300

    Начал делать "сложную" задачу домашки - конвертер студентов.

---
+++ Lesson8Homework/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentCSV2XML_Drop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void Rectangle_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+                Console.WriteLine(files[0]);
+            }
+        }
+
+        private void StudentCSV2XML_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //сделать изменение размеров области приёма файлов
+        }
+    }
+}
---
+++ Lesson8Homework/WpfApp1/StudentSerializable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    [Serializable]
+    class StudentSerializable
+    {
+        string firstName;
+        string lastName;
+        string univercity;
+        string faculty;
+        string profession;
+        int age;
+        int cource;
+        int group;
+        int city;
+    }
+}
